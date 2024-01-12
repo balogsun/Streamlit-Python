581e08bff156c6caa6a7bfdb69dcea725bd22042
Ubuntu <ubuntu@Demo.riajqc4z40nu3dpr3gctps4iad.ux.internal.cloudapp.net>
2024-01-12 23:56:37 +0000

    first commit18

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -18,7 +18,7 @@ AWS_ACCOUNT_ID='570580688935'
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 
 # pull the new image
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/xtb-mo-st-app:latest
+docker pull 570580688935.dkr.ecr.ca-central-1.amazonaws.com/webapp:latest
 
 # run the container
-docker run -d -p 8501:8501 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/xtb-mo-st-app:latest
+docker run -d -p 8501:8501 570580688935.dkr.ecr.ca-central-1.amazonaws.com/webapp:latest
